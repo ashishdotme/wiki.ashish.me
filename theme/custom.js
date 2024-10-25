3570fef1e77024061a5a45600fae105776248928
Ashish Patel <ashishsushilpatel@gmail.com>
2024-10-25 17:04:56 +0100

    chore: update custom.js

--- theme/custom.js
+++ theme/custom.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const chapterToggles = Array.from(document.querySelectorAll('.chapter li > a.toggle div'))
 
   chapterToggles.forEach((el) => {
-    el.innerHTML = '&gt;'
+    el.innerHTML = '>'
     el.style.visibility = 'visible'
   })
 })
