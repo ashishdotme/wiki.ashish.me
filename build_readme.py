f9e8c3fd97a9be1f2b2bf9b74b0bac61d630ea1b
Ashish Patel <ashishsushilpatel@gmail.com>
2024-11-11 22:52:47 +0000

    feat: add index

--- build_readme.py
+++ build_readme.py
@@ -2,13 +2,21 @@ import pathlib
 import sqlite_utils
 import sys
 import re
+import fileinput
 
 root = pathlib.Path(__file__).parent.resolve()
 
 index_re = re.compile(
     r"<!\-\- index starts \-\->.*<!\-\- index ends \-\->", re.DOTALL)
 
+def replaceAll(file,searchExp,replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
+
 if __name__ == "__main__":
+    replaceAll( root / "SUMMARY.md","[wiki.ashish.me](readme.md)","[wiki.ashish.me](index.md)")
     db = sqlite_utils.Database(root / "notes.db")
     by_topic = {}
     for row in db["notes"].rows_where(order_by="updated_utc desc"):
---
+++ index.md
@@ -0,0 +1,11 @@
+# wiki.ashish.me
+
+my personal wiki
+
+## Topics
+
+Main topics covered by this wiki:
+
+- [Software Development](./software-development/index.html)
+- [Personal](./personal/index.html)
+- [System Design](./system-design/index.html)
